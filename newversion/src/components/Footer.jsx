f114054bca51fe325e364e268a496b3d93cc4cdd
Devasundari <107170185+Devasundari@users.noreply.github.com>
2025-01-16 12:22:15 +0530

    Update Footer.jsx

--- newversion/src/components/Footer.jsx
+++ newversion/src/components/Footer.jsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import logo from '../assets/logo.png';
 import "../style.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCcVisa, faCcMastercard, faGooglePay, faFacebook, faTwitter, faInstagram, faYoutube,faGooglePlay } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
   return (
@@ -12,23 +14,23 @@ function Footer() {
             <img src={logo} alt="logo" className="logo" />
             <h2>DreamikAI</h2>
           </a>
-          <p><strong>Address:</strong> MURVEN Infotech Design Solutions LLP<br />
-            715-A, 7th Floor, Spencer Plaza,<br />
-            Suite No.548, Mount Road, Anna Salai,<br />
-            Chennai - 600 002, Tamil Nadu, India</p>
-          <p><strong>General Inquiries:</strong> +91-44-28505188</p>
-          <p><strong>Email:</strong> murven.design@gmail.com</p>
-          <p><strong>GST:</strong> 33ABPFM6846A1Z8</p>
-          <div className="follow">
-            <h4>Follow us</h4>
-            <div className="icons">
-              <i className="fab fa-facebook-f"></i>
-              <i className="fab fa-twitter"></i>
-              <i className="fab fa-instagram"></i>
-              <i className="fab fa-pinterest-p"></i>
-              <i className="fab fa-youtube"></i>
-            </div>
+
+          <div className="contact-info">
+            <p><strong>Address:</strong>
+             MURVEN Infotech Design Solutions LLP,
+             715-A,7th Floor,Spencer Plaza,
+             Suite No.548,Mount Road,Anna Salai,
+             Chennai-600 002,Tamil Nadu,India.
+            </p>
+            <p><strong>General Inquiries:</strong> +91-44-28505188</p>
+            <span><strong>Email:</strong>
+            <a data-auto-recognition="true" href="mailto:murven.design@gmail.com" class="wixui-rich-text__text">murven.design@gmail.com</a>
+            </span>
+            <p><strong>GST:</strong> 33ABPFM6846A1Z8</p>
+            
           </div>
+
+          
         </div>
         <div className="col2">
           <h3>Menu</h3>
@@ -36,7 +38,7 @@ function Footer() {
           <a href="privacy.html">Privacy Policy</a>
           <a href="terms.html">Terms & Conditions</a>
           <a href="refund.html">Return and Refund</a>
-          <a href="#">Contact Us</a>
+          <a href="#"><i className="fa-solid fa-phone"></i>Contact Us</a>
         </div>
         <div className="col3">
           <h3>My Account</h3>
@@ -48,21 +50,33 @@ function Footer() {
           <a href="https://docs.google.com/forms/d/e/1FAIpQLSemg6VNtaJAAbcxAqCs7U8w5pBkv5-QgHSi1SNf-9dC7z_ueA/viewform?vc=0&c=0&w=1&flr=0&usp=mail_form_link">Help</a>
           <a href="https://docs.google.com/forms/d/e/1FAIpQLScO4MlvWy3ZuLNy1e_aifz7EP-Lfypva2nc6mgzOTVFLnGHlw/viewform?vc=0&c=0&w=1&flr=0">Feedback</a>
         </div>
-        <div className="col install">
-          <h3>Install</h3>
-          <p>From Google Play</p>
-          <button>
-            Get it on<br />
-            <i className="fa-brands fa-google-play" style={{ color: 'red' }}></i>Google Play
-          </button>
-          <p>Secured Payment Gateways</p>
-          <ul>
-            <li>Visa</li>
-            <li>Mastercard</li>
-            <li>Maestro</li>
-            <li>Gpay</li>
-          </ul>
+      
+        <div className="follow">
+            <h3>Follow us</h3>
+            <div className="icons">
+              <a href="https://www.facebook.com/dreamikai"><FontAwesomeIcon icon={faFacebook} /></a>
+              <a href="https://twitter.com/dreamikaicomics"><FontAwesomeIcon icon={faTwitter} /></a>
+              <a href="https://www.instagram.com/dreamik.ai/"><FontAwesomeIcon icon={faInstagram} /></a>
+              <a href="https://www.youtube.com/channel/UC4B8UinlrPeW4yY0yPc37Tg"><FontAwesomeIcon icon={faYoutube} /></a>
+            
+            </div>
         </div>
+        <div className="col-install">
+           <h3>Install</h3>
+           <div className="icons">
+              <a href="https://play.google.com/store/apps/details?id=com.murvenllp.dreamikaicomics"><FontAwesomeIcon icon={faGooglePlay} /></a>
+           </div>
+        </div>  
+        <div className="col-payment">
+          <h3>Secured Payment Gateways</h3>
+          <div className="icons">
+            <a href=''><FontAwesomeIcon icon={faCcVisa} /></a>
+            <a href=''><FontAwesomeIcon icon={faCcMastercard} /></a>
+            <a href=''>M</a>
+            <a href=''><FontAwesomeIcon icon={faGooglePay} /></a>
+          </div>
+        </div>
+        
       </footer>
       <footer className="f-2">
         <p>© 2024 by Dreamik AI. Created by Sanads Digital</p>
