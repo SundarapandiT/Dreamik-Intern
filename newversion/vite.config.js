cecfef4ebe271aa11524d87645ee9a2b7c4b2cca
Sundarapandi T <131521909+SundarapandiT@users.noreply.github.com>
2025-01-16 20:07:28 +0530

    Update vite.config.js

--- newversion/vite.config.js
+++ newversion/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        '@fortawesome/react-fontawesome',
+        '@fortawesome/free-solid-svg-icons',
+        '@fortawesome/free-brands-svg-icons',
+        '@fortawesome/fontawesome-svg-core',
+      ],
+    },
+  },
 })
