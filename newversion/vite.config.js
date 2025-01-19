e5372a6fca255b24e4155c863b68c0eba7184cd4
Sundarapandi T <131521909+SundarapandiT@users.noreply.github.com>
2025-01-19 19:31:13 +0530

    Update vite.config.js

--- newversion/vite.config.js
+++ newversion/vite.config.js
@@ -5,15 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      external: [
-        '@fortawesome/react-fontawesome',
-        '@fortawesome/free-solid-svg-icons',
-        '@fortawesome/free-brands-svg-icons',
-        '@fortawesome/fontawesome-svg-core',
-        'jszip',
-        'html2canvas',
-      ],
-    },
+    chunkSizeWarningLimit: 1000, // Set to a higher value, e.g., 1000 KB
   },
-})
+});
