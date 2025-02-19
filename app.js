fb89bda75b8bbfba79bf0e136276063f69ab322b
Sundarapandi T <131521909+SundarapandiT@users.noreply.github.com>
2025-02-19 18:35:05 +0530

    Update app.js /userlogs 4

--- app.js
+++ app.js
@@ -660,10 +660,6 @@ app.post("/api/log", async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
-
-
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
